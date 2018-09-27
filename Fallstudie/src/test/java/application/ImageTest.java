1291b84a38901055dee666bf62cc9d71aedaadae
ichrist97 <43031508+ichrist97@users.noreply.github.com>
2018-09-27 10:28:03 +0200

    First Test

---
+++ Fallstudie/src/test/java/application/ImageTest.java
@@ -0,0 +1,20 @@
+package application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class ImageTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testBerechneBewertungImage() {
+		fail("Not yet implemented");
+	}
+
+}
